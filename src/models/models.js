11c2cf44d451771ee7b5855c9c22b9e72f9131e8
Bakayoko Marc Ezechiel <bakezechiel3231@gmail.com>
2024-09-09 22:02:10 +0200

    Class Practitionner

--- src/models/models.js
+++ src/models/models.js
@@ -1,5 +1,47 @@
 // Définition des classes JavaScript pour les utilisateurs de base de base
-
-
-
-export {}
+class Practitioner {
+    constructor(resourceType, identifier, name, telecom, gender, birthDate, qualification, address) {
+      this.resourceType = resourceType || "Practitioner";
+      this.identifier = identifier || [];
+      this.name = name || [];
+      this.telecom = telecom || [];
+      this.gender = gender || "";
+      this.birthDate = birthDate || "";
+      this.qualification = qualification || [];
+      this.address = address || [];
+    }
+  
+    setIdentifier(use, system, value) {
+      this.identifier.push({ use, system, value });
+    }
+  
+    setName(use, family, given, suffix) {
+      this.name.push({ use, family, given, suffix });
+    }
+  
+    setTelecom(system, value, use) {
+      this.telecom.push({ system, value, use });
+    }
+  
+    setGender(gender) {
+      this.gender = gender;
+    }
+  
+    setBirthDate(birthDate) {
+      this.birthDate = birthDate;
+    }
+  
+    setQualification(system, code, display) {
+      this.qualification.push({
+        code: { coding: [{ system, code, display }] }
+      });
+    }
+  
+    setAddress(use, line, city, postalCode, country) {
+      this.address.push({ use, line, city, postalCode, country });
+    }
+  }
+  
+export {
+    Practitioner
+}
